c5ddd41562b619de89ae462dfa95442b84cd942e
Sufyan <sufianb.contact@gmail.com>
2023-06-02 09:24:34 +0300

    Add Navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Navbar from './components/Navbar.jsx'
 function App() {
   return (
-    <div>
-
-    </div>
+    <>
+      <Navbar />
+    </>
   );
 }
 
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,63 @@
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import Offcanvas from 'react-bootstrap/Offcanvas';
+
+function OffcanvasExample() {
+  return (
+    <>
+        {['lg'].map((expand) => (
+        <Navbar key={expand} bg="light" expand={expand} className="mb-3">
+            <Container fluid>
+                <Navbar.Brand href="#">Logo</Navbar.Brand>
+                <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-${expand}`} />
+                <Navbar.Offcanvas
+                    id={`offcanvasNavbar-expand-${expand}`}
+                    aria-labelledby={`offcanvasNavbarLabel-expand-${expand}`}
+                    placement="end"
+                >
+                <Offcanvas.Header closeButton>
+                <Offcanvas.Title id={`offcanvasNavbarLabel-expand-${expand}`}>
+                    Logo
+                </Offcanvas.Title>
+                </Offcanvas.Header>
+                <Offcanvas.Body>
+                    <Nav className="justify-content-end flex-grow-1 pe-3">
+                        <Nav.Link href="/">Home</Nav.Link>
+                        <NavDropdown
+                            title="Filters"
+                            id={`offcanvasNavbarDropdown-expand-${expand}`}
+                        >
+                        <NavDropdown.Item>Action</NavDropdown.Item>
+                        <NavDropdown.Item>
+                            Filter1
+                        </NavDropdown.Item>
+                        <NavDropdown.Divider />
+                        <NavDropdown.Item>
+                            Filter2
+                        </NavDropdown.Item>
+                        </NavDropdown>
+                    </Nav>
+                    <Form className="d-flex">
+                        <Form.Control
+                        type="search"
+                        placeholder="Search"
+                        className="me-2"
+                        aria-label="Search"
+                    />
+                        <Button className="me-4" variant="outline-success">Search</Button>
+                    </Form>
+                    <h6>Cart</h6>
+                    </Offcanvas.Body>
+                </Navbar.Offcanvas>
+            </Container>
+        </Navbar>
+        ))}
+    </>
+    );
+}
+
+export default OffcanvasExample;
